627147c9b762c070900f8f78578f1504ebf35dc5
leonardo <leonardoperdigao78@gmail.com>
2026-06-10 12:51:41 +0000

    feat: Incrementation and start of validation by token decorator.

---
+++ app/auth.py
@@ -0,0 +1,20 @@
+from functools import wraps
+from flask import request, current_app
+import jwt
+
+def token_obrigatorio(f):
+    @wraps(f)
+    def wrapper(*args, **kwargs):
+        auth = request.headers.get('Authorization')
+        if not auth:
+            return{"error":"Token invalido ou inexistente."} , 401
+        token = auth.split(' ') [1]
+        try:
+            payload = jwt.decode(token, current_app.config['SECRET_KEY'], algorithms=["HS256"])
+        except:
+            return {"error":"Token inválido."}
+
+        return f(*args, **kwargs)
+    return wrapper
+
+
