1ddc0ae5cdf0eb20c389fc1b65bc2ceb085245bd
leonardo <leonardoperdigao78@gmail.com>
2026-06-11 12:09:23 +0000

    Feat CRUD projects completed

--- app/projects.py
+++ app/projects.py
@@ -43,3 +43,46 @@ def listar_projetos (user_id):
 
         })
     return {"projetos" : projects_list}
+
+
+@projects_bp.route('/projetos/<int:id>', methods=['GET'])
+@token_obrigatorio
+def buscar(user_id, id):
+    projeto = Project.query.get(id)
+    if not projeto:
+        return {"error": "projeto não encontrado ou não criado."}
+    else: 
+        return {
+    "id": projeto.id,
+    "name": projeto.name,
+    "desc": projeto.desc,
+    "created_at": projeto.created_at
+    }
+
+@projects_bp.route('/projetos/<int:id>' , methods=['PUT'])
+@token_obrigatorio
+def atualizar (user_id, id):
+    dados = request.get_json()
+    projeto = Project.query.get(id)
+    if not projeto:
+        return {"error": "projeto não existe."}, 404
+    name = dados.get('name')
+    desc = dados.get('desc')
+    if name:
+        projeto.name = name
+    if desc:
+        projeto.desc = desc
+
+    db.session.commit()
+    return {"message": "Projeto atualizado com sucesso!"}
+
+
+@projects_bp.route('/projetos/<int:id>' , methods=['DELETE'])
+@token_obrigatorio
+def deletar_projeto (user_id, id):
+    projeto = Project.query.get(id)
+    if not projeto:
+        return {"error": "projeto não existe."}, 404
+    db.session.delete(projeto)
+    db.session.commit()
+    return {"message": "Projeto removido com sucesso!"}
